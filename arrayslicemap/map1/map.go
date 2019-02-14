8b14e235230be86b5e2ceebb2cf87d5f1cc58107
fernando.gonzalez <fernando.stoiano@gmail.com>
2019-02-14 18:02:50 -0200

    Map

---
+++ arrayslicemap/map1/map.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// var aprovados map[int]string
+	// mapas devem ser inicializados
+	aprovados := make(map[int]string)
+
+	aprovados[12345678978] = "Maria"
+	aprovados[98765432100] = "Pedro"
+	aprovados[95135745682] = "Ana"
+	fmt.Println(aprovados)
+
+	for cpf, nome := range aprovados {
+		fmt.Printf("%s (CPF: %d)\n", nome, cpf)
+	}
+
+	fmt.Printf(aprovados[95135745682])
+	delete(aprovados, 95135745682)
+	fmt.Printf(aprovados[95135745682])
+}
